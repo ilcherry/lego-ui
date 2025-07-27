6712ac87f40fa48dee9c7e760a7c272ee6dce656
perry <perry.gao@callnovo.com>
2025-07-27 21:18:11 +0800

    Comment out ESLint patch and remove unused import in Button component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+// require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
@@ -23,4 +23,4 @@ module.exports = {
     node: true,
     browser: true
   }
-} 
+}
--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -35,7 +35,6 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import type { ButtonProps } from './types'
 
 const props = withDefaults(defineProps<ButtonProps>(), {
